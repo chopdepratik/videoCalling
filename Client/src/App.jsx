083666761601c48fe89bba8af1811600f3ddbcdf
Pratik Chopde <chopdepratik60@gmail.com>
2025-04-17 23:54:24 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://videocalling-sqzh.onrender.com");
 
 const VideoCall = () => {
   const localVideo = useRef();
